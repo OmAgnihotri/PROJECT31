cb5be820ccbedc95d464889a4bac912b00f4d565
OmAgnihotri <72681406+OmAgnihotri@users.noreply.github.com>
2020-12-23 18:39:42 +0530

    Add files via upload

--- particle.js
+++ particle.js
@@ -1,13 +1,12 @@
 class Particle{
-    constructor(x,y,r){
+    constructor(x,y){
         var options={
-            isStatic:false,
-            density:0.8,
+            restitution:0.4
             
             
         }
-        this.r=r;
-        this.body=Bodies.circle(x,y,this,r,this.r,options);
+        this.r=10;
+        this.body=Bodies.circle(x,y,10,options);
         this.color=color(random(0,255),random(0,255),random(0,255));
         World.add(world,this.body);
 
